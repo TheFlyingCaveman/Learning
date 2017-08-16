81b1bf164b886cec19def915b8a1c1703b3240aa
Joshua Miller <joshdan65@gmail.com>
2017-08-15 21:33:55 -0500

    Initial commit of CallingAProtectedAzureApi

---
+++ CallingProtectedAzureApis/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Identity.Client;
+using System.Windows;
+
+namespace CallingAProtectedAzureApi
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static string ClientId = "SomeClientId";
+
+        public static PublicClientApplication PublicClientApp = new PublicClientApplication(ClientId);
+    }
+}
---
+++ CallingProtectedAzureApis/README.md
@@ -0,0 +1,3 @@
+This was created using the guided setup from part one of https://docs.microsoft.com/en-us/azure/app-service-mobile/app-service-mobile-how-to-configure-microsoft-authentication
+
+Possible next steps: https://docs.microsoft.com/en-us/azure/app-service-api/app-service-api-dotnet-service-principal-auth
