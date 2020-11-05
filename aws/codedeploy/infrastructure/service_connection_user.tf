8bb83d48e9b6a26e5269da8be4b801d05dde5b0a
Joshua Miller <joshdan65@gmail.com>
2020-11-04 21:07:48 -0600

    Add service connection user infra

---
+++ aws/codedeploy/infrastructure/service_connection_user.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_group" "codedeploygroup" {
+  name = "Testing_CodeDeploy_Access"
+  path = "/testing_codedeploy/"
+}
+
+resource "aws_iam_group_policy_attachment" "codedeploygroup_ec2" {
+  group      = aws_iam_group.codedeploygroup.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "codedeploygroup_s3" {
+  group      = aws_iam_group.codedeploygroup.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "codedeploygroup_codedeploy" {
+  group      = aws_iam_group.codedeploygroup.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployFullAccess"
+}
+
+resource "aws_iam_user" "codedeploy" {
+  name = "AzureDevOps_Service_Connection"
+  path = "/testing_codedeploy/"
+
+  tags = {
+    usage = "Azure DevOps"
+  }
+}
+
+resource "aws_iam_user_group_membership" "codedeploy" {
+  user = aws_iam_user.codedeploy.name
+
+  groups = [
+    aws_iam_group.codedeploygroup.name
+  ]
+}
