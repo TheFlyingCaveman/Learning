36b9a66ded036fb4e5ea3e0ca6670c9825f82e74
Joshua Miller <joshdan65@gmail.com>
2020-11-06 20:11:59 -0600

    Parameterize resources

--- aws/codedeploy/infrastructure/output.tf
+++ aws/codedeploy/infrastructure/output.tf
@@ -2,10 +2,6 @@ output "instance_public_dns" {
   value = [aws_instance.web.*.public_dns]
 }
 
-output "instance_public_ip" {
-  value = [aws_instance.web.*.public_ip]
-}
-
 output "lb_dns" {
   value = "http://${aws_lb.main.dns_name}"
 }
