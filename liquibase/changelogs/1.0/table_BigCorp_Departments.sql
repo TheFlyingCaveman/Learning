b9d9ea9006751058c6d9185194af790a51b20ef6
Joshua Miller <joshdan65@gmail.com>
2021-01-06 10:25:13 -0600

    Add table

---
+++ liquibase/changelogs/1.0/table_BigCorp_Departments.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+--changeset id:1324 author:JoshuaMiller
+--comment: Departments encapsulates BigCorp departments
+CREATE TABLE BigCorp.Departments (
+    name VARCHAR(50)
+)
+--rollback DROP TABLE BigCorp.Departments
