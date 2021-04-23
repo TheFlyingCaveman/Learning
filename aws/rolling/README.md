732a9b50d46c5755f8f364062332fca22f8377f8
Joshua Miller <joshdan65@gmail.com>
2021-04-22 22:59:50 -0500

    Fix image pulling after making ECS private

--- aws/rolling/README.md
+++ aws/rolling/README.md
@@ -8,3 +8,8 @@
     * Or, how to keep containers private while still leveraging Docker Hub (at the time of writing, item #5)
     * > For Auto-assign Public IP, choose whether to have your tasks receive a public IP address. If you are using Fargate tasks, in order for the task to pull the container image it must either use a public subnet and be assigned a public IP address or a private subnet that has a route to the internet or a NAT gateway that can route requests to the internet.
 * [ECS with Private ECR](https://www.easydeploy.cloud/blog/how-to-create-private-link-for-ecr-to-ecs-containers-to-save-nat-gatewayec2-other-charges/)
+    * [Amazon ECS interface VPC endpoints (AWS PrivateLink)](https://docs.aws.amazon.com/AmazonECS/latest/developerguide/vpc-endpoints.html)
+
+## TODOS
+
+- [ ] Apply policies to Private Links [to further restrict access](https://docs.aws.amazon.com/AmazonECR/latest/userguide/vpc-endpoints.html)
--- aws/rolling/infrastructure/main.tf
+++ aws/rolling/infrastructure/main.tf
@@ -23,6 +23,11 @@ resource "aws_cloudwatch_log_group" "ecs" {
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+
+  tags = local.standard_tags
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
 
 resource "aws_internet_gateway" "gw" {
@@ -193,6 +198,133 @@ resource "aws_ecs_cluster" "api" {
 #   protocol = "HTTP"
 #   vpc_id   = aws_vpc.main.id
 # }
+
+resource "aws_security_group" "from_ecs_tasks" {
+  name        = "${local.service_name}-from-ecs-tasks"
+  description = "Allow inbound access from ECS"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+    # security_groups = [aws_security_group.ecs_tasks.id]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.region}.ecr.dkr"
+  vpc_endpoint_type = "Interface"
+
+  auto_accept = true
+
+  private_dns_enabled = true
+
+  security_group_ids = [
+    aws_security_group.from_ecs_tasks.id
+  ]
+
+  subnet_ids = aws_subnet.private.*.id
+
+  tags = merge(
+    local.standard_tags,
+    {
+      Name = "${local.service_name}-ecr-dkr"
+  })
+}
+
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.region}.ecr.api"
+  vpc_endpoint_type = "Interface"
+
+  auto_accept = true
+
+  private_dns_enabled = true
+
+  security_group_ids = [
+    aws_security_group.from_ecs_tasks.id
+  ]
+
+  subnet_ids = aws_subnet.private.*.id
+
+  tags = merge(
+    local.standard_tags,
+    {
+      Name = "${local.service_name}-ecr-api"
+  })
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.region}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  auto_accept = true
+
+  route_table_ids = [aws_route_table.public.id]
+
+  # security_group_ids = [
+  #   aws_security_group.from_ecs_tasks.id
+  # ]
+
+  # subnet_ids = aws_subnet.private.*.id
+
+  tags = merge(
+    local.standard_tags,
+    {
+      Name = "${local.service_name}-s3"
+  })
+}
+
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.region}.logs"
+  vpc_endpoint_type = "Interface"
+
+  auto_accept = true
+
+  security_group_ids = [
+    aws_security_group.from_ecs_tasks.id
+  ]
+
+  subnet_ids = aws_subnet.private.*.id
+
+  tags = merge(
+    local.standard_tags,
+    {
+      Name = "${local.service_name}-logs"
+  })
+}
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.region}.ssm"
+  vpc_endpoint_type = "Interface"
+
+  auto_accept = true
+
+  security_group_ids = [
+    aws_security_group.from_ecs_tasks.id
+  ]
+
+  subnet_ids = aws_subnet.private.*.id
+
+  tags = merge(
+    local.standard_tags,
+    {
+      Name = "${local.service_name}-ssm"
+  })
+}
 
 resource "aws_ecs_service" "api" {
   name        = local.service_name
