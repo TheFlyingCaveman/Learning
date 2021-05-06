90dee571721678136b8e6e49e4695afff960ee61
Joshua Miller <joshdan65@gmail.com>
2021-05-06 18:50:42 -0500

    Fix: Add missing listener

--- aws/rolling/infrastructure/modules/internal_containerized_service/loadbalancer.tf
+++ aws/rolling/infrastructure/modules/internal_containerized_service/loadbalancer.tf
@@ -70,6 +70,17 @@ resource "aws_lb_target_group" "main" {
   }
 }
 
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"    
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
 # module "pretty_domain" {
 #   source = "../pretty_domain"
 
